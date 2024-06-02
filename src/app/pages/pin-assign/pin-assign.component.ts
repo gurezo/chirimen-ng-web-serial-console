8787e1a41d18860ffc8d87dee5be15ab10cc3630
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-02 15:08:46 +0900

    component pin assign add

---
+++ src/app/pages/pin-assign/pin-assign.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pin-assign',
+  standalone: true,
+  imports: [],
+  templateUrl: './pin-assign.component.html',
+  styleUrl: './pin-assign.component.scss'
+})
+export class PinAssignComponent {
+
+}
