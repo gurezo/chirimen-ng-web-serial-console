77794833a0be9a8bab587e06a42ac156b0cdf0d7
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-02 15:09:02 +0900

    component wifi assign add

---
+++ src/app/pages/wifi-setting/wifi-setting.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WifiSettingComponent } from './wifi-setting.component';
+
+describe('WifiSettingComponent', () => {
+  let component: WifiSettingComponent;
+  let fixture: ComponentFixture<WifiSettingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WifiSettingComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(WifiSettingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/wifi-setting/wifi-setting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wifi-setting',
+  standalone: true,
+  imports: [],
+  templateUrl: './wifi-setting.component.html',
+  styleUrl: './wifi-setting.component.scss'
+})
+export class WifiSettingComponent {
+
+}
