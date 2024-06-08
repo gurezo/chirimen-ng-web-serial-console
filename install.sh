60b47718b799d3afb8e18bc02becaf600631fa40
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-08 23:14:18 +0900

    new install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "portfolio re-install"
+echo "chirimen-ng-web-serial-console re-install"
 echo ""
 
-echo "remove node_modules, package-lock.json, .angular .firebase"
-rm -rf node_modules package-lock.json .angular .firebase
+echo "remove node_modules, package-lock.json, .angular"
+rm -rf node_modules package-lock.json .angular
 
 echo ""
 echo "npm cache verify"
