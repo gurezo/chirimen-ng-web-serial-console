b26419a1a06ee6f05a164fd0d8457c2efb87e011
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-09 13:48:08 +0900

    path add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { browserCheckGuard } from './guards/browser-check.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    canActivate: [browserCheckGuard],
+  },
+  {
+    path: 'unsupported-browser',
+    loadChildren: () =>
+      import('./pages/unsupported-browser/unsupported-browser.component'),
+  },
+];
