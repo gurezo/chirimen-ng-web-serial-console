6e79cd6d617cfba5cbbf5b6e08034a7a0cc577d1
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-09 21:37:06 +0900

    refactor guards

--- src/app/guards/browser-check.guard.ts
+++ src/app/guards/browser-check.guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const browserCheckGuard: CanActivateFn = (route, state) => {
+export const browserCheckGuard: CanActivateFn = () => {
   const router = inject(Router);
 
   const userAgent = window.navigator.userAgent.toLowerCase();
@@ -12,8 +12,10 @@ export const browserCheckGuard: CanActivateFn = (route, state) => {
   const isOpera = /opr/.test(userAgent);
 
   if (isChrome || isEdge || isOpera) {
-    return true; // アクセスを許可
+    // デスクトップ版の Chrome, Edge, Opera の場合はページ遷移を許可
+    return true;
   } else {
+    // それ以外のブラウザの場合は、サポート外ページへ遷移
     router.navigate(['/unsupported-browser']);
     return false;
   }
