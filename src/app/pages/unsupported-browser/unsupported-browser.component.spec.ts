7683240909fb034eefe23e12bba996ec4855c6b0
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-09 13:37:22 +0900

    rename to unsupported-browser

---
+++ src/app/pages/unsupported-browser/unsupported-browser.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UnsupportedBrowserComponent } from './unsupported-browser.component';
+
+describe('UnsupportedBrowserComponent', () => {
+  let component: UnsupportedBrowserComponent;
+  let fixture: ComponentFixture<UnsupportedBrowserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UnsupportedBrowserComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UnsupportedBrowserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/unsupported-browser/unsupported-browser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unsupported-browser',
+  standalone: true,
+  imports: [],
+  templateUrl: './unsupported-browser.component.html',
+  styleUrl: './unsupported-browser.component.scss'
+})
+export class UnsupportedBrowserComponent {
+
+}
