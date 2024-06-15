22daa56aadd436f29bc3c58570131caadd4c3ae6
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-15 16:10:11 +0900

    feat(editor): temporary editor impliments

--- src/app/containers/editor-containers/editor-containers.component.ts
+++ src/app/containers/editor-containers/editor-containers.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MonacoEditorService } from '../../services/monaco-editor.service';
 
 @Component({
   selector: 'app-editor-containers',
   standalone: true,
   imports: [],
   templateUrl: './editor-containers.component.html',
-  styleUrl: './editor-containers.component.scss'
+  styleUrl: './editor-containers.component.scss',
+  providers: [MonacoEditorService],
 })
 export class EditorContainersComponent {
+  private monaca = inject(MonacoEditorService);
 
+  loadFile(): void {}
+  save(): void {}
+  create(): void {}
+  delete(): void {}
+  rename(): void {}
 }
