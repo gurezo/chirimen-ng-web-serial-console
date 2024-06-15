6c5f7e62f406537e0cb5ed24d974fa6c4dd66cf1
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-15 15:02:45 +0900

    feat(sservice): generate service

---
+++ src/app/services/monaco-editor.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MonacoEditorService {
+  constructor() {}
+
+  /**
+   * ngx-monaco-editor-v2 を使った処理を実装する
+   */
+
+  // TODO: ファイルの読み込み
+  // TODO: ファイルの保存
+  // TODO: ファイルの新規作成
+  // TODO: ファイルの削除
+  // TODO: ファイルのリネーム
+}
---
+++ src/app/services/wi-fi.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WiFiService {
+  /**
+   * Wi-Fi 情報をあ使うサービスを実装する
+   */
+  constructor() {}
+
+  // TODO: Wi-Fi リスト情報取得
+  // TODO: Wi-Fi 設定(SSID, PassWord)情報取得
+}
