ab8d8bd94b02a4525652a3cf6a0031a359bc9490
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-29 23:01:25 +0900

    add monaco-editor, sample code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import {
+  NgxMonacoEditorConfig,
+  provideMonacoEditor,
+} from 'ngx-monaco-editor-v2';
+import { routes } from './app.routes';
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  baseUrl: '/assets',
+  defaultOptions: {
+    scrollBeyondLastLine: false,
+    baseUrl: '/assets',
+  },
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideMonacoEditor(monacoConfig),
+  ],
 };
--- src/app/containers/editor-containers/editor-containers.component.ts
+++ src/app/containers/editor-containers/editor-containers.component.ts
@@ -1,16 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MonacoEditorService } from '@app/services';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 @Component({
   selector: 'app-editor-containers',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MonacoEditorModule],
   templateUrl: './editor-containers.component.html',
   styleUrl: './editor-containers.component.scss',
   providers: [MonacoEditorService],
 })
-export class EditorContainersComponent {
+export class EditorContainersComponent implements OnInit {
   private monaca = inject(MonacoEditorService);
+  editorOptions = { theme: 'vs-dark', language: 'javascript' };
+  code: string = 'function x() {\nconsole.log("Hello world!");\n}';
+
+  ngOnInit(): void {
+    // let line = editor.getPosition();
+    // console.log(line);
+  }
 
   loadFile(): void {}
   save(): void {}
