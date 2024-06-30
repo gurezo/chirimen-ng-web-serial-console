1b4038ea3e1e3652379e93aa19a7b5ac1e6953a1
akihiko.kigure <akihiko.kigure@gmail.com>
2024-06-30 14:57:19 +0900

    replace config

---
+++ jest.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from 'jest';
+
+const esmPackages = ['@angular/*', '@testing-library/angular'];
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^@constants/(.*)$': '<rootDir>/src/app/constants/$1',
+    '^@containers/(.*)$': '<rootDir>/src/app/containers/$1',
+    '^@guards/(.*)$': '<rootDir>/src/app/guards/$1',
+    '^@pages/(.*)$': '<rootDir>/src/app/pages/$1',
+    '^@services/(.*)$': '<rootDir>/src/app/services/$1',
+  },
+};
+
+export default config;
