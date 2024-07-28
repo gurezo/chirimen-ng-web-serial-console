262443a30c6b6c088f0aec5a48f01ea55db1a107
akihiko.kigure <akihiko.kigure@gmail.com>
2024-07-28 15:46:04 +0900

    feat: web serial store update

--- src/app/shared/store/reducers/web-serial.reducers.ts
+++ src/app/shared/store/reducers/web-serial.reducers.ts
@@ -2,7 +2,7 @@ import { createReducer, on } from '@ngrx/store';
 import * as WebSerialActions from '../actions/web-serial.actions';
 import { WebSerialState } from '../models';
 
-export const initialState: WebSerialState = {
+export const initialWebSerialState: WebSerialState = {
   isConnect: false,
   ports: [],
   port: undefined,
@@ -11,7 +11,7 @@ export const initialState: WebSerialState = {
 export const webSerialFeatureKey = 'webSerial';
 
 export const webSerialReducer = createReducer(
-  initialState,
+  initialWebSerialState,
   on(WebSerialActions.initPort, (state) => ({
     ...state,
   })),
@@ -22,6 +22,6 @@ export const webSerialReducer = createReducer(
   })),
   on(WebSerialActions.disConnectPort, (state) => ({
     ...state,
-    initialState,
+    initialWebSerialState,
   }))
 );
