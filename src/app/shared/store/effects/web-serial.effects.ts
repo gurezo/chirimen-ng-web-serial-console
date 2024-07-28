c116be523908fed1c63f1fdf7b9710cea670dbba
akihiko.kigure <akihiko.kigure@gmail.com>
2024-07-28 17:47:01 +0900

    feat: add web serial effect temporry impliments

--- src/app/shared/store/effects/web-serial.effects.ts
+++ src/app/shared/store/effects/web-serial.effects.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { WebSerialService } from '@app/shared/service';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import * as WebSerialActions from '../actions/web-serial.actions';
 
 @Injectable()
-export class WebSerialEffects {}
+export class WebSerialEffects {
+  actions$ = inject(Actions);
+  service = inject(WebSerialService);
+
+  init$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(WebSerialActions.initPort)
+      // TODO: ここでシリアルポートのリクエストを行う
+      //  exhaustMap(() => from(this.service.requestPort()).pipe(
+      //   map(() => of())
+      // ))),
+      // map(() => from(this.service.requestPort())),
+      //  { dispatch: false },
+      // switchMap(() => {
+      //   return this.service.requestPort().then(
+      //     () => {},
+      //     error((error) => console.error(error))
+      //   );
+      // })
+    )
+  );
+}
