7f67a6c06561def9672442d12c89d96d9570089f
akihiko.kigure <akihiko.kigure@gmail.com>
2024-08-02 16:09:07 +0900

    budgets size change

--- src/app/shared/store/reducers/xterm.reducers.ts
+++ src/app/shared/store/reducers/xterm.reducers.ts
@@ -1,10 +1,15 @@
 import { createReducer, on } from '@ngrx/store';
-import { Terminal } from '@xterm/xterm';
 import * as XTermActions from '../actions/xterm.actions';
 import { XTermState } from '../models';
 
 export const initialXTermState: XTermState = {
-  terminal: new Terminal(),
+  /**
+   * TODO: new Terminal() を定義すると下記ワーニング発生する
+   * ▲ [WARNING] bundle initial exceeded maximum budget. Budget 1.05 MB was not met by 39.78 kB with a total of 1.09 MB.
+   * ▲ [WARNING] Module '@xterm/xterm' used by 'src/app/shared/store/reducers/xterm.reducers.ts' is not ESM
+   * 暫定で、null で初期化しておく
+   */
+  terminal: null,
   wirteData: '',
   readData: '',
   currentDirectory: '',
