65abb6151970dd96545d41fa85f7b297d3c3e6ff
akihiko.kigure <akihiko.kigure@gmail.com>
2024-08-03 15:06:49 +0900

    feat: generate tree component

---
+++ apps/web-serial-console/src/app/shared/components/tree/tree.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tree',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tree.component.html',
+  styleUrl: './tree.component.css',
+})
+export class TreeComponent {}
