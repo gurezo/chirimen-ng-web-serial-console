e699094c6199ff60b9d67eeed9cda507c81bb48e
akihiko.kigure <akihiko.kigure@gmail.com>
2024-08-03 16:13:56 +0900

    feat: create model for file type

---
+++ apps/web-serial-console/src/app/shared/models/directory.info.models.ts
@@ -0,0 +1,26 @@
+// cf. https://medium.com/@EchoLilt/types-of-files-in-linux-fc621eb1a0cb
+type FileType =
+  | 'regular file'
+  | 'directory'
+  | 'character device file'
+  | 'block device file'
+  | 'local socket file'
+  | 'named pipe'
+  | 'symbolic link';
+
+module FileType {
+  export const REGULAR_FILE: FileType = 'regular file';
+  export const DIRECTORY: FileType = 'directory';
+  export const CHARACTER_DEVICE_FILE: FileType = 'character device file';
+  export const BLOCK_DEVICE_FILE: FileType = 'block device file';
+  export const LOCAL_SOCKET_FILE: FileType = 'local socket file';
+  export const NAMED_PIPE: FileType = 'named pipe';
+  export const SYMBOLIC_LINK: FileType = 'symbolic link';
+}
+
+export interface FileInfo {
+  name: string;
+  type: FileType;
+  size: number;
+  child: FileInfo[];
+}
