b2e493f3b2b386e411e2424f6ca405f10702e8e4
akihiko.kigure <akihiko.kigure@gmail.com>
2024-08-04 16:12:05 +0900

    feat: temporary impliments

--- apps/web-serial-console/src/app/shared/components/tree/tree.component.ts
+++ apps/web-serial-console/src/app/shared/components/tree/tree.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+import { FileInfo } from '../../models';
 
 @Component({
   selector: 'app-tree',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatTreeModule, MatIconModule],
   templateUrl: './tree.component.html',
-  styleUrl: './tree.component.css',
+  styleUrl: './tree.component.scss',
 })
-export class TreeComponent {}
+export class TreeComponent {
+  @Input() treeControl!: FlatTreeControl<FileInfo>;
+  @Input() dataSource!: FileInfo[];
+  hasChild = (_: number, _nodeData: any) => _nodeData.expandable;
+}
