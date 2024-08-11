607d08e7ff0a8ce30a02fdaec82dfe86469686c3
akihiko.kigure <akihiko.kigure@gmail.com>
2024-08-11 16:54:23 +0900

    feat: temporary impliments

--- apps/web-serial-console/src/app/shared/service/web-serial/web-serial.service.ts
+++ apps/web-serial-console/src/app/shared/service/web-serial/web-serial.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
-import { SerialPort } from 'web-serial-polyfill';
-// TODOL: インポート文の実装を再検討
-// import {
-//   serial as polyfill,
-//   SerialPort as SerialPortPolyfill,
-// } from 'web-serial-polyfill';
+// TODO: この import はどうするか
+// 参照自体は、下記パスで出来る
+// node_modules/.pnpm/@types+w3c-web-serial@1.0.6/node_modules/@types/w3c-web-serial/index.d.ts
+// import { SerialPort } from 'web-serial-polyfill';
 
 @Injectable({
   providedIn: 'root',
@@ -12,77 +10,68 @@ import { SerialPort } from 'web-serial-polyfill';
 export class WebSerialService {
   // TODO: シリアルポートの状態管理をどうするか
   // NgRx のルートストアに登録するか、サービス内で管理するか
-  serialPort: SerialPort | undefined;
-  navSerialPort: any;
+  serialPort: SerialPort | undefined = undefined;
+  encoder = new TextEncoder();
+  lang: string = 'en';
 
-  // async requestPort(): Promise<void> {
-  //   try {
-  //     const port = await navigator.serial.requestPort();
-  //     const connect = await port.open({ baudRate: 115200 });
-  //     console.log('port OK', connect);
-  //   } catch (error) {
-  //     console.log('error is', error);
-  //     // console.error(error);
-  //   }
-  // }
-  async requestPort(): Promise<void> {
-    try {
-      this.navSerialPort = await navigator.serial.requestPort();
-    } catch (error) {
-      // 未選択時の仕様をどうするか？
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    if (navigator.language == 'ja') {
+      this.lang = navigator.language;
     }
   }
 
-  // TODO: 一応仮実装、使い道あるかどうか？
-  async getPorts(): Promise<void> {
+  async requestPort(): Promise<SerialPort | null> {
     try {
-      const ports = await navigator.serial.getPorts();
-      console.log('port info: ', ports);
-    } catch (error) {}
+      this.serialPort = await navigator.serial.requestPort();
+      await this.serialPort.open({ baudRate: 115200 });
+      console.log('this.serialPort', this.serialPort);
+      return this.serialPort;
+    } catch (error) {
+      return null;
+    }
   }
 
-  connectPort() {
-    this.navSerialPort.open({ baudRate: 115200 });
+  portWrite(data: any): boolean {
+    if (!this.serialPort) {
+      return false;
+    }
+
+    try {
+      const writer = this.serialPort?.writable?.getWriter();
+      writer?.write(this.encoder.encode(data));
+      writer?.releaseLock();
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
   }
 
-  async closePort() {
+  portRead() {
+    if (!this.serialPort) {
+      return false;
+    }
+
+    try {
+      const reader = this.serialPort?.readable?.getReader();
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  async closePort(): Promise<boolean> {
     try {
       this.serialPort?.close();
-    } catch (error) {}
+    } catch (error) {
+      console.error(error);
+    } finally {
+      return true;
+    }
   }
-
-  /**
-   *
-   *
-   *
-   *
-  memo
-  PiZeroWebSerialConsole code
-
-  onconnect: ((this: this, ev: Event) => any) | null;
-  ondisconnect: ((this: this, ev: Event) => any) | null;
-
-  getPorts(): Promise<SerialPort[]>;
-  requestPort(options?: SerialPortRequestOptions): Promise<SerialPort>;
-  addEventListener(
-    type: 'connect' | 'disconnect',
-    listener: (this: this, ev: Event) => any,
-    useCapture?: boolean
-  ): void;
-  addEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject | null,
-    options?: boolean | AddEventListenerOptions
-  ): void;
-  removeEventListener(
-    type: 'connect' | 'disconnect',
-    callback: (this: this, ev: Event) => any,
-    useCapture?: boolean
-  ): void;
-  removeEventListener(
-    type: string,
-    callback: EventListenerOrEventListenerObject | null,
-    options?: EventListenerOptions | boolean
-  ): void;
-   */
 }
