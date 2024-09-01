ebc9adae131382c0356532094484b96702bfe5dc
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-01 14:49:11 +0900

    feat : replace  createAction to createActionGroup for xterm

--- apps/web-serial-console/src/app/shared/store/actions/xterm.actions.ts
+++ apps/web-serial-console/src/app/shared/store/actions/xterm.actions.ts
@@ -1,12 +1,11 @@
-import { createAction, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
-export const init = createAction('[XTerm] initialize');
-export const write = createAction(
-  '[XTerm] Write',
-  props<{ wirteData: string }>()
-);
-export const read = createAction('[XTerm] Read', props<{ readData: string }>());
-export const currentDirectory = createAction(
-  '[XTerm] Current Directory',
-  props<{ currentDirectory: string }>()
-);
+export const WebSerialActions = createActionGroup({
+  source: '[XTerm]',
+  events: {
+    init: emptyProps(),
+    currentDirectory: props<{ currentDirectory: string }>(),
+    read: props<{ readData: string }>(),
+    write: props<{ wirteData: string }>(),
+  },
+});
