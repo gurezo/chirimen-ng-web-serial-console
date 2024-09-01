c8041f44c4358f339eadeaebd754179f4ce778d1
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-01 15:57:57 +0900

    feat: impliments iframe component

--- apps/web-serial-console/src/app/containers/pin-assign-containers/pin-assign-containers.component.ts
+++ apps/web-serial-console/src/app/containers/pin-assign-containers/pin-assign-containers.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ExampleFrameComponent } from '../../shared';
 
 @Component({
   selector: 'app-pin-assign-containers',
   standalone: true,
-  imports: [],
+  imports: [ExampleFrameComponent],
   templateUrl: './pin-assign-containers.component.html',
-  styleUrl: './pin-assign-containers.component.scss'
+  styleUrl: './pin-assign-containers.component.scss',
 })
-export class PinAssignContainersComponent {
-
-}
+export class PinAssignContainersComponent {}
