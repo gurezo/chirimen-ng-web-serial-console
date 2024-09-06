c38c4d0d4fc585e70064733171b6f0a9707fa220
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-06 16:44:43 +0900

    feat: pin-assign page button add

--- apps/web-serial-console/src/app/containers/pin-assign-containers/pin-assign-containers.component.ts
+++ apps/web-serial-console/src/app/containers/pin-assign-containers/pin-assign-containers.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
-import { ExampleFrameComponent } from '../../shared';
+import { RouterLink } from '@angular/router';
+import { ButtonComponent, ExampleFrameComponent } from '../../shared';
+
+const Mode = {
+  SETUP: 'setup',
+  I2C: 'i2c',
+  RESIDENT: 'resident',
+  EXAMPLE: 'example',
+};
+type Mode = (typeof Mode)[keyof typeof Mode];
 
 @Component({
   selector: 'app-pin-assign-containers',
   standalone: true,
-  imports: [ExampleFrameComponent],
+  imports: [ButtonComponent, ExampleFrameComponent, RouterLink],
   templateUrl: './pin-assign-containers.component.html',
   styleUrl: './pin-assign-containers.component.scss',
 })
-export class PinAssignContainersComponent {}
+export class PinAssignContainersComponent {
+  pageMode = Mode;
+  mode: string = Mode.SETUP;
+
+  setUpChirimen() {
+    this.mode = this.pageMode.SETUP;
+  }
+  i2cDetect() {
+    this.mode = this.pageMode.I2C;
+  }
+  getExample() {
+    this.mode = this.pageMode.EXAMPLE;
+  }
+  residentAppConf() {
+    this.mode = this.pageMode.RESIDENT;
+  }
+}
