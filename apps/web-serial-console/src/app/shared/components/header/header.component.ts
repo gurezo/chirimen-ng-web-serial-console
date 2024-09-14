d73100d58f9d326c0df5d9da08a946fe7353dc12
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-14 16:37:53 +0900

    feat: add header

--- apps/web-serial-console/src/app/shared/components/header/header.component.ts
+++ apps/web-serial-console/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatIconModule, MatMenuModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
