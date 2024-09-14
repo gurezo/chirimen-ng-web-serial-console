717ed542ab6db48941aaac2204f1cc25c5149121
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-14 16:38:01 +0900

    feat: add header

--- apps/web-serial-console/src/app/app.component.ts
+++ apps/web-serial-console/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [HeaderComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
