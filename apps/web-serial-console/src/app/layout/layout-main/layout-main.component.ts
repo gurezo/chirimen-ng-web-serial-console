5f1defddcb5152c8322bfac6c63663b85163b047
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-16 13:46:34 +0900

    feat: generate layout

---
+++ apps/web-serial-console/src/app/layout/layout-main/layout-main.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout-main.component.html',
+  styleUrl: './layout-main.component.scss',
+})
+export class LayoutMainComponent {}
