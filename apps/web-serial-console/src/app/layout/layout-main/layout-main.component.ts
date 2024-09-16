60a9da5f07c339f5e7bc6050eb9a261542d5080a
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-16 14:13:46 +0900

    feat: replace layout

--- apps/web-serial-console/src/app/layout/layout-main/layout-main.component.ts
+++ apps/web-serial-console/src/app/layout/layout-main/layout-main.component.ts
@@ -1,11 +1,42 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  ConsoleContainersComponent,
+  EditorContainersComponent,
+} from '../../containers';
+import {
+  BreadcombComponent,
+  ButtonComponent,
+  TreeComponent,
+  WebSerialActions,
+  WebSerialService,
+} from '../../shared';
 
 @Component({
   selector: 'app-layout-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    BreadcombComponent,
+    ButtonComponent,
+    ConsoleContainersComponent,
+    EditorContainersComponent,
+    TreeComponent,
+  ],
   templateUrl: './layout-main.component.html',
   styleUrl: './layout-main.component.scss',
 })
-export class LayoutMainComponent {}
+export default class LayoutMainComponent {
+  store = inject(Store);
+  service = inject(WebSerialService);
+
+  label = 'connect';
+  mode = 'editor';
+
+  ngOnInit() {
+    this.store.dispatch(WebSerialActions.init());
+  }
+
+  onClick() {
+    this.store.dispatch(WebSerialActions.init());
+  }
+}
