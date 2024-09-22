9b10951910883c5b1bc73444f671d3df22d9f366
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-22 15:01:39 +0900

    feat: remove page type

--- apps/web-serial-console/src/app/shared/models/page.type.models.ts
+++
@@ -1,8 +0,0 @@
-export const PageType = {
-  console: 'console',
-  editor: 'editor',
-  wifiSetting: 'wifi-setting',
-  pinAssign: 'pin-assign',
-} as const;
-
-export type PageType = (typeof PageType)[keyof typeof PageType];
