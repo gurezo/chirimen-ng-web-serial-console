64cc97d515a0665ee149fea1935428c27f8e8154
akihiko.kigure <akihiko.kigure@gmail.com>
2024-09-22 14:51:47 +0900

    feat: terminal icon add

--- apps/web-serial-console/src/app/shared/components/header/header.component.ts
+++ apps/web-serial-console/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -10,4 +11,16 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  private domSanitizer = inject(DomSanitizer);
+  private matIconRegistry = inject(MatIconRegistry);
+
+  ngOnInit(): void {
+    this.matIconRegistry.addSvgIcon(
+      'terminal',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '/terminal_24dp_5F6368.svg',
+      ),
+    );
+  }
+}
