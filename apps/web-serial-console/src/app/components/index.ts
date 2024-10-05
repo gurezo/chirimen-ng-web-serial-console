684592f1e58511f19a72a796ffc09eafb8db8eff
akihiko.kigure <akihiko.kigure@gmail.com>
2024-10-05 19:49:49 +0900

    refactor: move component to dialog

--- apps/web-serial-console/src/app/components/index.ts
+++ apps/web-serial-console/src/app/components/index.ts
@@ -1,5 +1,4 @@
 export * from './breadcomb/breadcomb.component';
 export * from './button/button.component';
-export * from './example-frame/example-frame.component';
 export * from './pin-assign/pin-assign.component';
 export * from './tree/tree.component';
--- apps/web-serial-console/src/app/dialog/index.ts
+++ apps/web-serial-console/src/app/dialog/index.ts
@@ -1,9 +1,11 @@
 import { ChirimenSetupComponent } from './chirimen-setup/chirimen-setup.component';
+import { ExampleFrameComponent } from './example-frame/example-frame.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { I2cdetectComponent } from './i2cdetect/i2cdetect.component';
 
 const dialogs = [
   ChirimenSetupComponent,
+  ExampleFrameComponent,
   FileUploadComponent,
   I2cdetectComponent,
 ];
@@ -11,6 +13,7 @@ const dialogs = [
 export {
   ChirimenSetupComponent,
   dialogs,
+  ExampleFrameComponent,
   FileUploadComponent,
   I2cdetectComponent,
 };
