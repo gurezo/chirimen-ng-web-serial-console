a73dc4e75824262d2d331de8daf1eeabadf2ce27
akihiko.kigure <akihiko.kigure@gmail.com>
2024-10-05 22:23:51 +0900

    refactor: move component to dialog

--- apps/web-serial-console/src/app/containers/index.ts
+++ apps/web-serial-console/src/app/containers/index.ts
@@ -1,16 +1,6 @@
 import { ConsoleContainersComponent } from './console-containers/console-containers.component';
 import { EditorContainersComponent } from './editor-containers/editor-containers.component';
-import { WifiSettingComponent } from './wifi-setting/wifi-setting.component';
 
-const containers = [
-  ConsoleContainersComponent,
-  EditorContainersComponent,
-  WifiSettingComponent,
-];
+const containers = [ConsoleContainersComponent, EditorContainersComponent];
 
-export {
-  ConsoleContainersComponent,
-  containers,
-  EditorContainersComponent,
-  WifiSettingComponent,
-};
+export { ConsoleContainersComponent, containers, EditorContainersComponent };
--- apps/web-serial-console/src/app/dialog/index.ts
+++ apps/web-serial-console/src/app/dialog/index.ts
@@ -2,12 +2,14 @@ import { ChirimenSetupComponent } from './chirimen-setup/chirimen-setup.componen
 import { ExampleFrameComponent } from './example-frame/example-frame.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { I2cdetectComponent } from './i2cdetect/i2cdetect.component';
+import { WifiSettingComponent } from './wifi-setting/wifi-setting.component';
 
 const dialogs = [
   ChirimenSetupComponent,
   ExampleFrameComponent,
   FileUploadComponent,
   I2cdetectComponent,
+  WifiSettingComponent,
 ];
 
 export {
@@ -16,4 +18,5 @@ export {
   ExampleFrameComponent,
   FileUploadComponent,
   I2cdetectComponent,
+  WifiSettingComponent,
 };
