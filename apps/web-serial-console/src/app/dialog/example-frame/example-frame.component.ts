bd91d87e723878db1e9e09908d578e04c3822450
akihiko.kigure <akihiko.kigure@gmail.com>
2024-10-06 16:51:49 +0900

    refactor: example dialog button

--- apps/web-serial-console/src/app/dialog/example-frame/example-frame.component.ts
+++ apps/web-serial-console/src/app/dialog/example-frame/example-frame.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ButtonComponent } from '../../components';
+import { DialogService } from '../../service/dialog/dialog.service';
 
 @Component({
   selector: 'app-example-frame',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './example-frame.component.html',
   styleUrl: './example-frame.component.scss',
 })
-export class ExampleFrameComponent {}
+export class ExampleFrameComponent {
+  private service = inject(DialogService);
+
+  closeModal(): void {
+    this.service.closeDialog();
+  }
+}
