3d673a486eb750a3c32b25fb764f1c15062f31bb
akihiko.kigure <akihiko.kigure@gmail.com>
2024-10-14 14:46:06 +0900

    feat: add dialog create file

---
+++ apps/web-serial-console/src/app/dialog/create-file/create-file.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-file',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-file.component.html',
+  styleUrl: './create-file.component.scss',
+})
+export class CreateFileComponent {}
--- apps/web-serial-console/src/app/dialog/index.ts
+++ apps/web-serial-console/src/app/dialog/index.ts
@@ -1,4 +1,5 @@
 import { ChirimenSetupComponent } from './chirimen-setup/chirimen-setup.component';
+import { CreateFileComponent } from './create-file/create-file.component';
 import { ExampleFrameComponent } from './example-frame/example-frame.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { I2cdetectComponent } from './i2cdetect/i2cdetect.component';
@@ -6,6 +7,7 @@ import { WifiSettingComponent } from './wifi-setting/wifi-setting.component';
 
 const dialogs = [
   ChirimenSetupComponent,
+  CreateFileComponent,
   ExampleFrameComponent,
   FileUploadComponent,
   I2cdetectComponent,
@@ -14,6 +16,7 @@ const dialogs = [
 
 export {
   ChirimenSetupComponent,
+  CreateFileComponent,
   dialogs,
   ExampleFrameComponent,
   FileUploadComponent,
