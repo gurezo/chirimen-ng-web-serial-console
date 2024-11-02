815e941800dd7013533d051787a24cd0dd08879a
akihiko.kigure <akihiko.kigure@gmail.com>
2024-11-02 23:03:11 +0900

    feat: add diver for console

--- apps/web-serial-console/src/app/components/console-tool-bar/console-tool-bar.component.ts
+++ apps/web-serial-console/src/app/components/console-tool-bar/console-tool-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-console-tool-bar',
   standalone: true,
-  imports: [MatIconModule, MatMenuModule],
+  imports: [MatDividerModule, MatIconModule, MatMenuModule],
   templateUrl: './console-tool-bar.component.html',
   styleUrl: './console-tool-bar.component.scss',
 })
