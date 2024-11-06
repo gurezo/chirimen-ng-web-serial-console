396035e799d4e51e96a9ef2d644062b11c49f574
akihiko.kigure <akihiko.kigure@gmail.com>
2024-11-06 23:01:45 +0900

    feat: update example service

--- apps/web-serial-console/src/app/service/example/example.service.ts
+++ apps/web-serial-console/src/app/service/example/example.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ExampleJson } from '../../models';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { Observable } from 'rxjs';
 export class ExampleService {
   http = inject(HttpClient);
 
-  getJsonArray<T>(path: string): Observable<Array<T>> {
-    return this.http.get<Array<T>>(path);
+  getJsonArray(path: string): Observable<Array<ExampleJson>> {
+    return this.http.get<Array<ExampleJson>>(path);
   }
 }
