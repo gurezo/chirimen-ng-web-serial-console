7a434c8d91c4edfc220c50cab2a7d3718628fe04
akihiko.kigure <akihiko.kigure@gmail.com>
2024-11-17 19:53:34 +0900

    feat: update docs sh

--- docs.sh
+++ docs.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "change branch"
+echo ""
+git checkout gh-pages
+
+
+echo "dashboard build"
+echo ""
+
+npx nx build apps-dashboard --prod --output-path docs --base-href /chirimen-lite-dashboard/
+
 echo "dashboard deploy"
 echo ""
 
-mv -rf dist/chirimen-ng-web-serial-console/browser docs
+mv docs/browser/* docs/
