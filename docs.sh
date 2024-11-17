f7fc7d44dca91e285ff6f9793df16e028f38c914
akihiko.kigure <akihiko.kigure@gmail.com>
2024-11-17 20:32:07 +0900

    feat: update docs sh

--- docs.sh
+++ docs.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 set -e
 
+echo "branch change gh-pages"
+echo ""
+
+git checkout gh-pages
+
 echo "dashboard build"
 echo ""
 
 npx nx build apps-dashboard --prod --base-href /chirimen-lite-dashboard/
 
 
-echo "dashboard docs clean"
+echo "dashboard docs remove"
 echo ""
 
 rm -rf docs
 
-echo "dashboard deploy"
+echo "dashboard docs create"
+echo ""
+
+mkdir docs
+
+
+echo "dashboard deploy code move"
 echo ""
 
 mv -f dist/dashboard/browser docs
+
+echo "git add"
+echo ""
+
+git add .
+
+echo "git commit"
+echo ""
+
+git commit -m 'new source'
+
+echo "git push"
+echo ""
+
+git push origin gh-pages -f
+
+echo "branch change main"
+echo ""
+
+git checkout gh-pages
