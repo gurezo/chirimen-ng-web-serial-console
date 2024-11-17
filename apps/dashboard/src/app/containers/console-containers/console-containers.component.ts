e18748dc3e0d0be3b8b265a63ac55b14e58e18f0
akihiko.kigure <akihiko.kigure@gmail.com>
2024-11-17 23:19:45 +0900

    feat: add demo console

--- apps/dashboard/src/app/containers/console-containers/console-containers.component.ts
+++ apps/dashboard/src/app/containers/console-containers/console-containers.component.ts
@@ -61,8 +61,8 @@ export class ConsoleContainersComponent implements AfterViewInit {
       return;
     }
 
-    this.xterminal.reset();
-    this.xterminal.writeln('$ ');
+    // this.xterminal.reset();
+    // this.xterminal.writeln('$ ');
 
     this.xterminal.onKey((e) => this.xtermService.onKey(this.xterminal, e));
 
@@ -70,38 +70,38 @@ export class ConsoleContainersComponent implements AfterViewInit {
   }
 
   private async demoConsole() {
-    await sleep(3000);
     this.xterminal.reset();
     this.xterminal.writeln('<<CONNECTED>');
     this.xterminal.writeln('Waiting promp');
     this.xterminal.writeln('.....');
+    this.xterminal.writeln(' ');
 
     await sleep(3000);
-    this.xterminal.writeln('raspberrypi login: p');
+    this.xterminal.writeln('raspberrypi login: pi');
     this.xterminal.writeln('Password:');
+    this.xterminal.writeln(' ');
 
     await sleep(3000);
     this.xterminal.writeln(
       'Linux raspberrypi 6.6.31+rpt-rpi-v6 #1 Raspbian 1:6.6.31-1+rpt1 (2024-05-29) armv6l',
-    ),
-      this.xterminal.writeln(' ');
+    );
+    this.xterminal.writeln(' ');
     this.xterminal.writeln(
       'The programs included with the Debian GNU/Linux system are free software;',
-    ),
-      this.xterminal.writeln(
-        'the exact distribution terms for each program are described in the individual files in /usr/share/doc/*/copyright',
-      );
+    );
+    this.xterminal.writeln(
+      'the exact distribution terms for each program are described in the individual files in /usr/share/doc/*/copyright',
+    );
     this.xterminal.writeln(' ');
 
     await sleep(3000);
     this.xterminal.writeln(
       'Debian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent',
     );
-    this.xterminal.writeln('permitted by applicable law'),
-      this.xterminal.writeln(
-        'Last login: Sat Nov 16 17:58:38 JST 2024 on ttyGS',
-      );
+    this.xterminal.writeln('permitted by applicable law');
+    this.xterminal.writeln('Last login: Sat Nov 16 17:58:38 JST 2024 on ttyGS');
     this.xterminal.writeln('pi@raspberrypi:~$ HISTCONTROL=ignorebot');
+    this.xterminal.writeln(' ');
 
     await sleep(3000);
     this.xterminal.writeln(
@@ -113,29 +113,27 @@ export class ConsoleContainersComponent implements AfterViewInit {
     await sleep(3000);
     this.xterminal.writeln('pi@raspberrypi:~$ pw');
     this.xterminal.writeln('/home/p');
+    this.xterminal.writeln(' ');
 
     await sleep(1000);
     this.xterminal.writeln('pi@raspberrypi:~$ cd -');
     this.xterminal.writeln('pi@raspberrypi:~$ ls -al --quoting-style=');
     this.xterminal.writeln('合計 4');
-    this.xterminal.writeln('drwx------ 6 pi pi 4096 9月 16 14:57 ".'),
-      this.xterminal.writeln('drwxr-xr-x 3 root root 4096 8月 21 12:13 "..'),
-      this.xterminal.writeln(
-        '-rw------- 1 pi pi 34 9月 16 14:57 ".bash_history',
-      );
+    this.xterminal.writeln('drwx------ 6 pi pi 4096 9月 16 14:57 ".');
+    this.xterminal.writeln('drwxr-xr-x 3 root root 4096 8月 21 12:13 "..');
+    this.xterminal.writeln('-rw------- 1 pi pi 34 9月 16 14:57 ".bash_history');
     this.xterminal.writeln('-rw-r--r-- 1 pi pi 220 8月 21 12:13 ".bash_logout');
-    this.xterminal.writeln('-rw-r--r-- 1 pi pi 3523 8月 21 12:13 ".bashrc'),
-      this.xterminal.writeln('drwxr-xr-x 3 pi pi 4096 8月 21 12:22 ".cache'),
-      this.xterminal.writeln('drwx------ 4 pi pi 4096 10月 5 14:51 ".forever');
-    this.xterminal.writeln('drwxr-xr-x 4 pi pi 4096 8月 21 12:21 ".npm'),
-      this.xterminal.writeln('-rw-r--r-- 1 pi pi 807 8月 21 12:13 ".profile'),
-      this.xterminal.writeln(
-        '-rw-r--r-- 1 pi pi 0 8月 21 12:30 ".sudo_as_admin_successful"',
-      );
-    this.xterminal.writeln('drwxr-xr-x 3 pi pi 4096 11月 10 16:40 "myApp'),
-      this.xterminal.writeln(
-        '-rwxr-xr-x 1 pi pi 463 9月 1 15:11 "wifi_setup.sh',
-      );
+    this.xterminal.writeln('-rw-r--r-- 1 pi pi 3523 8月 21 12:13 ".bashrc');
+    this.xterminal.writeln('drwxr-xr-x 3 pi pi 4096 8月 21 12:22 ".cache');
+    this.xterminal.writeln('drwx------ 4 pi pi 4096 10月 5 14:51 ".forever');
+    this.xterminal.writeln('drwxr-xr-x 4 pi pi 4096 8月 21 12:21 ".npm');
+    this.xterminal.writeln('-rw-r--r-- 1 pi pi 807 8月 21 12:13 ".profile');
+    this.xterminal.writeln(
+      '-rw-r--r-- 1 pi pi 0 8月 21 12:30 ".sudo_as_admin_successful"',
+    );
+    this.xterminal.writeln('drwxr-xr-x 3 pi pi 4096 11月 10 16:40 "myApp');
+    this.xterminal.writeln('-rwxr-xr-x 1 pi pi 463 9月 1 15:11 "wifi_setup.sh');
+    this.xterminal.writeln(' ');
 
     await sleep(1000);
     this.xterminal.writeln('pi@raspberrypi:~$');
